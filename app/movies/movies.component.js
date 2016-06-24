f348a303947dd2cf27834388caa5b7fb8c8bf162
srinivasan <kksrini89@gmail.com>
2016-06-25 00:28:03 +0530

    $http service is implemented with angularjs component

--- app/movies/movies.component.js
+++ app/movies/movies.component.js
@@ -1,6 +1,27 @@
 /**
  * Created by srini on 24-06-2016.
  */
-(function(){
-    var app = angular.module('');
+(function () {
+    var app = angular.module('movies');
+
+    function controller($http) {
+        var model = this;
+        model.$onInit = function () {
+            fetchMovies($http).then(function (data) {
+                model.movies = data;
+            });
+        }
+    }
+
+    function fetchMovies($http) {
+        return $http.get('Data/movies.json').then(function (response) {
+            return response.data;
+        });
+    }
+
+    app.component('moviesList', {
+        templateUrl: 'movies/movies.component.html',
+        controllerAs: 'model',
+        controller: ['$http', controller]
+    });
 }());
