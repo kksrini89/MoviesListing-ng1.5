15c0302b9d314f16237b872e8bcfd10afe61d025
srinivasan <kksrini89@gmail.com>
2016-06-26 12:59:45 +0530

    decoupled star rating from movie-list component

--- app/movies/movies-rating.component.js
+++ app/movies/movies-rating.component.js
@@ -4,26 +4,36 @@
 (function () {
     'use strict';
     var app = angular.module('movies');
-    function controller(){
+
+    function controller() {
         var model = this;
-        model.$onInit = function(){
-            model.entries = new Array(model.value);
+        model.$onInit = function () {
+            //model.entries = new Array(model.max);
+            model.entries = buildEntries(model.value,model.max);
         };
-        function log(){
-            console.log(model.value);
-        }
+
         model.$onChanges = function () {
-            model.entries = new Array(model.value);
+            model.entries = buildEntries(model.value,model.max);
         }
+    }
+
+    function buildEntries(value, max) {
+        var entries = [];
+        for (var i = 1; i <= max; i++) {
+            var entry = i<=value ? "fa-star":"fa-star-o";
+            entries.push(entry);
+        }
+        return entries;
     }
 
     app.component('moviesRating', {
         templateUrl: 'movies/movies-rating.component.html',
-        bindings : {
-            value : '<'
+        bindings: {
+            value: '<',
+            max: '<'
         },
-        transclude : true,
-        controllerAs : 'model',
+        //transclude : true,
+        controllerAs: 'model',
         controller: controller
     });
 }());
